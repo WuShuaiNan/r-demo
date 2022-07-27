e6d0a6a3a64305594088d7f636f7f086b082c9e7
wusn <wu_shuainan@qq.com>
2022-07-27 11:48:52 +0800

    创建项目，版本为 1.0.0.a-SNAPSHOT

---
+++ src/main/java/com/wusn/r/demo/RDemoApplication.java
@@ -0,0 +1,13 @@
+package com.wusn.r.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wusn/r/demo/RDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wusn.r.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
